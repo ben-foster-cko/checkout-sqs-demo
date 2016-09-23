e41addeeed92958dec86ff195999790017314f92
Ben Foster <ben.foster@checkout.com>
2016-09-23 17:46:22 +0100

    Initial Commit

---
+++ src/Demo.Receiver/Program.cs
@@ -0,0 +1,54 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Demo.Receiver
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true)
+                .Build();
+
+            Console.Title = configuration["ApplicationName"];
+
+
+            var profile = configuration["AWSProfile"];
+            
+            var credentials = new StoredProfileAWSCredentials(profile);
+            var sqsClient = new AmazonSQSClient(credentials, RegionEndpoint.EUWest1);
+
+            // Create queue if not exist
+
+            GetQueueUrlResponse queue = sqsClient.GetQueueUrlAsync("BFQueue").Result;
+
+            var receiveMessageRequest = new ReceiveMessageRequest
+            {
+                QueueUrl = queue.QueueUrl,
+                WaitTimeSeconds = 10,
+                VisibilityTimeout = 10
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Polling Queue");
+                var messagesResponse = sqsClient.ReceiveMessageAsync(receiveMessageRequest).Result;
+                foreach (var message in messagesResponse.Messages)
+                {
+                    Console.WriteLine($"Message: { message.Body }");
+
+                    var deleteMessageRequest = new DeleteMessageRequest(queue.QueueUrl, message.ReceiptHandle);
+                    DeleteMessageResponse result = sqsClient.DeleteMessageAsync(deleteMessageRequest).Result;
+                }
+            }
+
+        }
+    }
+}
---
+++ src/Demo.Sender/Program.cs
@@ -0,0 +1,39 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+
+namespace Demo.Sender
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var credentials = new StoredProfileAWSCredentials("default");
+            var sqsClient = new AmazonSQSClient(credentials, RegionEndpoint.EUWest1);
+
+            // Create queue
+
+            var createQueueRequest = new CreateQueueRequest
+            {
+                QueueName = "BFQueue"
+            };
+
+            CreateQueueResponse queue = sqsClient.CreateQueueAsync(createQueueRequest).Result;
+
+            Console.WriteLine("Send a message");
+
+            string message = null;
+            while((message = Console.ReadLine()) != "q")
+            {
+                var sendMessageRequest = new SendMessageRequest(queue.QueueUrl, message)
+                {
+                     
+                };
+
+                sqsClient.SendMessageAsync(sendMessageRequest);
+            }           
+        }
+    }
+}
